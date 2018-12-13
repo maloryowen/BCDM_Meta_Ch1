43ac5e3811fa6fb7252abac5978f1a5e8db5b46e
ewenharrison <maloryowen@gmail.com>
2018-12-13 13:41:07 -0500

    Create/fill in step files for meta, create/fill PRISMA script

---
+++ PRISMA_statement.R
@@ -0,0 +1,16 @@
+#PRISMA 
+#For workflow and documenting progress
+
+library(PRISMAstatement)
+help(prisma)
+
+PRISMA <- prisma(found=348,
+       found_other=0,
+       no_dupes=295,
+       screened=295,
+       screen_exclusions=202,
+       full_text=93,
+       full_text_exclusions=0,
+       qualitative=0,
+       quantitative=0)
+print(PRISMA)
